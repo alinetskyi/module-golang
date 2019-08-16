3e22d8770f4824a65151e0a40f3a97be18d496fb
Andrew Peresada <kohelbekker@gmail.com>
2019-08-16 13:48:52 +0300

    ex05

---
+++ ex05-parallel/Frequency.go
@@ -0,0 +1,25 @@
+package letter
+
+func FrequencyParallel(text string, c chan map[rune]int) {
+	c <- Frequency(text)
+}
+
+func Frequency(str string) map[rune]int {
+	m := make(map[rune]int)
+	for _, symbol := range str {
+		m[symbol]++
+	}
+	return m
+}
+
+func ConcurrentFrequency(text []string) map[rune]int {
+	c := make(chan map[rune]int)
+	new_m := make(map[rune]int)
+	for i := range text {
+		go FrequencyParallel(text[i], c)
+		for symbol, count := range <-c {
+			new_m[symbol] += count
+		}
+	}
+	return new_m
+}
